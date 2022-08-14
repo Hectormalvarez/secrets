d37935a3fb819bdad697d28cf949233bfcde0975
Hector <halvarez@taylormadetech.net>
2022-08-14 16:22:03 -0500

    added custom 8 grid template row tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '8': 'repeat(8, minmax(0, 1fr))'
+      }
+    },
   },
   plugins: [],
 }
