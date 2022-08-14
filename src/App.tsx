2af1d19abdb07ffff6998f7b721eabc0d5bb02d2
Hector <halvarez@taylormadetech.net>
2022-08-14 09:01:12 -0500

    added tailwind removed template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid grid-rows-6 h-screen'>
+      <header>Header</header>
+      <main className='row-span-5 bg-slate-400'>Main Content</main>
     </div>
   );
 }
