8d4c7a8b64dfefcbd6897ed04a862296f5e7de38
Hector <halvarez@taylormadetech.net>
2022-08-15 21:40:03 -0500

    changed app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import NewSecretForm from './components/NewSecretForm';
 
 
 function App() {
   return (
-    <div className='grid grid-rows-6 h-screen'>
-      <header>Header</header>
-      <main className='row-span-5 bg-slate-400'>Main Content</main>
+    <div className="grid grid-rows-8 h-screen font-mono">
+      <Header />
+      <main className="row-span-6 bg-slate-400"><NewSecretForm /></main>
+      <Footer />
     </div>
   );
 }
