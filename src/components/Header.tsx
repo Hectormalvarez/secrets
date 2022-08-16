eb7dfbef39164095950eef8dae3b588809a64f4f
Hector <halvarez@taylormadetech.net>
2022-08-15 21:40:38 -0500

    fixed text coloring in footer\header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='flex justify-center items-center'>
-      <h1 className='text-5xl font-mono font-bold text-slate-600'>Taylor Made Secrets</h1>
+    <header className='flex justify-center items-center bg-slate-200 dark:bg-slate-800'>
+      <h1 className='text-3xl font-mono font-bold text-slate-600 dark:text-slate-200'>Taylor Made Secrets</h1>
     </header>
   );
 }
