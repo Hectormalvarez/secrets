82828cb5c656b7128067e306fa587c9c1a98a33d
Hector <halvarez@taylormadetech.net>
2022-08-18 10:58:17 -0500

    added react-router; not complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import NewSecretForm from './components/NewSecretForm';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
+import Layout from "./components/Layout";
+import NewSecretForm from "./components/NewSecretForm";
 
 function App() {
   return (
-    <div className="grid grid-rows-8 h-screen font-mono">
-      <Header />
-      <main className="row-span-6 bg-slate-400"><NewSecretForm /></main>
-      <Footer />
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<NewSecretForm />} />
+        <Route path="/secret-created" element={<p>Created Secret</p>} />
+        <Route path="/open-secret" element={<p>open secret</p>} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function Layout(props: any) {
+  return (
+    <div className="grid h-screen grid-rows-8 font-mono">
+      <Header />
+      <main className="row-span-6 bg-slate-400">
+        {props.children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
