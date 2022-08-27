a92a9073de95e0159a6372f9a7cb59516be6c4a6
Hector <halvarez@taylormadetech.net>
2022-08-27 13:39:16 -0500

    new secret modal mostly complete

--- src/components/modal/NewSecretModal.tsx
+++ src/components/modal/NewSecretModal.tsx
@@ -1,83 +1,138 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useRef } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationIcon } from "@heroicons/react/outline";
+import { Fragment, useState, useRef } from "react";
 
-export default function NewSecretModal({
-  isOpen,
-  setIsOpen,
-  secretID
-}: any) {
-  const cancelButtonRef = useRef(null);
+import useCopy from "use-copy";
+
+import { Dialog, Transition } from "@headlessui/react";
+import { ClipboardCheckIcon, DocumentAddIcon } from "@heroicons/react/outline";
+
+export default function NewSecretModal({ isOpen, setIsOpen, secretID }: any) {
+  const [copied, copy, setCopied] = useCopy(
+    `https://secrets.taylormadetech.net/${secretID}`
+  );
+  const [copiedOnce, setCopiedOnce] = useState(false);
+  const [clickedOutsideModal, setClickedOutsideModal] = useState(false);
+  const secretLinkRef = useRef(null);
+
+  const handleCopyToClipboardClick = () => {
+    copySecretLink();
+  };
+
+  const copySecretLink = () => {
+    copy();
+    setCopiedOnce(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+
+  const handleClickOutsideModal = () => {
+    setClickedOutsideModal(true);
+
+    setTimeout(() => {
+      setClickedOutsideModal(false);
+    }, 3000);
+  };
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog
         as="div"
         className="relative z-10"
-        initialFocus={cancelButtonRef}
-        onClose={setIsOpen}
+        onClose={handleClickOutsideModal}
       >
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-out duration-500"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-500"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div
+            className="fixed inset-0 bg-gray-600 bg-opacity-90 transition-opacity"
+            aria-hidden="true"
+          />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-end justify-center p-4 text-center md:items-center md:p-0">
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enter="ease-out duration-500"
+              enterFrom="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
+              enterTo="opacity-100 translate-y-0 md:scale-100"
+              leave="ease-in duration-500"
+              leaveFrom="opacity-100 translate-y-0 md:scale-100"
+              leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationIcon
-                        className="h-6 w-6 text-red-600"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <Dialog.Panel className="relative overflow-hidden rounded-md transition-all md:my-8 md:w-full md:max-w-lg">
+                {copied && <p>Copied!</p>}
+                {clickedOutsideModal && !copiedOnce ? (
+                  <p>copy to clipboard to be able to create a new secret!</p>
+                ) : (
+                  ""
+                )}
+                {clickedOutsideModal && copiedOnce ? (
+                  <p>click create new secret to create a new secret!</p>
+                ) : (
+                  ""
+                )}
+                <div className="bg-slate-100 px-4 pt-5 pb-4 md:p-6 md:pb-4">
+                  <div className="md:flex md:items-start ">
+                    <div className="mt-3 text-center md:mt-0 md:ml-4 md:flex-grow md:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-lg font-medium leading-6 text-gray-900"
+                        className="text-3xl font-bold leading-6 text-gray-900"
                       >
                         New Secret Created!
                       </Dialog.Title>
-                      <div className="mt-2">
+                      <div className="mt-2 text-xl">
                         share your one time secret with this link!
                       </div>
+                      <input
+                        type="text"
+                        name="new-secret-link"
+                        id="new-secret-link"
+                        className="mt-2 w-full overflow-y-auto rounded-lg bg-slate-800 p-2 text-2xl text-slate-200 "
+                        value={`https://secrets.taylormadetech.net/${secretID}`}
+                        ref={secretLinkRef}
+                        readOnly
+                      />
                     </div>
                   </div>
                 </div>
-                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="bg-slate-800 px-4 py-3 md:flex md:flex-row-reverse md:px-6">
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setIsOpen(false)}
+                    className="
+                    md:text-md flex w-full items-center justify-center rounded-md border border-transparent bg-slate-500 px-4 py-2 text-white shadow-md
+                      hover:bg-blue-100 md:ml-3 md:w-auto md:hover:text-slate-700"
+                    onClick={handleCopyToClipboardClick}
                   >
-                    View Secret
+                    <p className="grow">Copy to Clipboard!</p>
+                    <ClipboardCheckIcon className="h-8 w-8" />
                   </button>
                   <button
                     type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={() => setIsOpen(false)}
-                    ref={cancelButtonRef}
+                    className={`
+                      mt-3 flex w-full items-center justify-center rounded-md border px-4 py-2 text-base
+                     md:mt-0 md:ml-3 md:w-auto 
+                     ${
+                       copiedOnce
+                         ? "border-gray-100 bg-gray-200 text-gray-700 md:cursor-pointer md:hover:bg-slate-300 md:hover:text-slate-900"
+                         : "border-gray-300 bg-gray-400 text-gray-500"
+                     }
+                    `}
+                    onClick={() => {
+                      setCopiedOnce(false);
+                      setIsOpen(false);
+                    }}
+                    disabled={!copiedOnce ? true : false}
                   >
-                    New Secret
+                    <p className="grow">New Secret!</p>
+                    <DocumentAddIcon className="h-8 w-8" />
                   </button>
                 </div>
               </Dialog.Panel>
--- src/pages/NewSecret.tsx
+++ src/pages/NewSecret.tsx
@@ -1,30 +1,26 @@
-import React, { BaseSyntheticEvent, useRef } from "react";
-import ReactDOM from "react-dom";
+import React, { BaseSyntheticEvent, useRef, useState } from "react";
 import NewSecretModal from "../components/modal/NewSecretModal";
+import { createID, encryptText } from "../utils";
 
-//import CryptoJS from "crypto-js";
-import { useState } from "react";
-
-// code to encrypt text
-//CryptoJS.AES.encrypt(
-//   e.target[0].value,
-//   process.env.REACT_APP_SECRET_KEY as string
-// ).toString()
-
-// code to decrypt
-// CryptoJS.AES.decrypt(
-//   e.target[0].value,
-//   process.env.REACT_APP_KEY as string
-// ).toString(CryptoJS.enc.Utf8);
+const initialSecret = {
+  secretID: "",
+  secretText: "",
+};
 
 const NewSecretForm = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [newSecret, setNewSecret] = useState(initialSecret);
+  const [textAreaLength, setTextAreaLength] = useState(0)
   const secretTextAreaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleNewSecretSubmit = (e: BaseSyntheticEvent) => {
     e.preventDefault();
-    console.log(secretTextAreaRef.current?.value);
     if (secretTextAreaRef && secretTextAreaRef.current) {
+      const encryptedSecret = encryptText(e.target.value, process.env.REACT_APP_KEY as string)
+      setNewSecret({
+        secretID: createID(),
+        secretText: encryptedSecret,
+      });
       secretTextAreaRef.current.value = "";
     }
     setModalIsOpen(true);
@@ -32,27 +28,33 @@ const NewSecretForm = () => {
 
   return (
     <>
-      {ReactDOM.createPortal(
-        <NewSecretModal isOpen={modalIsOpen} setIsOpen={setModalIsOpen} />,
-        document.getElementById("overlay-root") as HTMLElement
-      )}
+      <NewSecretModal
+        isOpen={modalIsOpen}
+        setIsOpen={setModalIsOpen}
+        secretID={newSecret.secretID}
+      />
       <form
-        className="m-2 flex h-full flex-col justify-end sm:justify-center md:px-8 lg:mx-auto lg:max-w-3xl"
+        className="m-2 flex h-full flex-col justify-end md:px-8 md:mx-auto md:max-w-3xl relative"
         onSubmit={handleNewSecretSubmit}
       >
-        <label className="text-xl text-slate-100">Enter a New Secret</label>
+        <label className="text-center font-bold tracking-wider text-3xl text-slate-800 md:text-4xl">
+          Enter a New Secret
+        </label>
         <textarea
-          className="mb-6 h-2/3 rounded-lg border-4 border-slate-800 bg-slate-200 p-2  sm:h-3/4"
-          placeholder="Enter New Secret Here"
+          className="mb-6 h-2/3 rounded-lg border-4 border-slate-800 bg-slate-100 p-4 text-lg md:h-3/4  md:text-2xl"
+          placeholder="Private information you want to share goes here"
           ref={secretTextAreaRef}
           required
+          onChange={e => setTextAreaLength(e.target.textLength)}
+          maxLength={500}
         />
         <button
           type="submit"
-          className="mb-6 bg-slate-300 p-2 text-xl hover:bg-slate-600 hover:text-slate-200 md:py-4"
+          className="mb-6 bg-slate-700 md:hover:bg-slate-400 md:hover:text-slate-100 md:hover:shadow-slate-700 py-4 text-2xl tracking-wider uppercase text-slate-200 shadow-lg shadow-slate-100 md:py-6 md:text-4xl md:font-bold"
         >
           Create New Secret
         </button>
+        <p className="absolute bottom-32 right-5 text-lg md:bottom-40 md:right-16 md:text-2xl">{textAreaLength}/500</p>
       </form>
     </>
   );
