74e2c78c7a1ba46dc7b171b13c2cb5975cbed366
Hector <halvarez@taylormadetech.net>
2022-08-27 13:39:05 -0500

    style changes to layout components

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-center bg-slate-200 font-mono font-bold text-slate-800  dark:bg-slate-800 dark:text-slate-200">
+    <footer className="text-md flex flex-col items-center justify-center font-mono bg-slate-800 text-slate-200  md:text-3xl md:tracking-wider">
       <p>by Taylor Made Technology Network</p>
       <p>2022</p>
     </footer>
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className='flex justify-center items-center bg-slate-200 dark:bg-slate-800'>
-      <h1 className='text-3xl font-mono font-bold text-slate-800 dark:text-slate-200'>Taylor Made Secrets</h1>
+    <header className='flex justify-center items-center bg-slate-800'>
+      <h1 className='text-3xl md:text-6xl font-mono font-bold text-slate-200'>Taylor Made Secrets</h1>
     </header>
   );
 }
