5ed41745ba5069d824d3d1a44bb51a9a64e0ff58
Hector <halvarez@taylormadetech.net>
2022-08-28 06:02:52 -0500

    added mock nanoid to opensecret test

--- src/pages/OpenSecret.test.js
+++ src/pages/OpenSecret.test.js
@@ -1,6 +1,10 @@
 import { render } from "@testing-library/react";
 import OpenSecret from "./OpenSecret";
 
+jest.mock("nanoid", () => {
+  return { nanoid: () => "1234" };
+});
+
 describe("<OpenSecret /> smoke test", () => {
     it("renders without error", () => {
         render(<OpenSecret />)
