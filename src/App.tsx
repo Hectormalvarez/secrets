52e675e37b3bccc44c1bccca8fae959b58a7d143
hadev <halvarez@taylormadetech.net>
2022-08-30 17:06:27 -0500

    renamed new secret route and added comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 
-import Layout from "./components/layout/Layout";
-import NewSecretForm from "./pages/NewSecret";
+import Layout from "./components/layout/Layout"; 
+
+import NewSecret from "./pages/NewSecret";
 import OpenSecret from "./pages/OpenSecret";
 
 function App() {
   return (
     <Layout>
       <Routes>
+        {/* Home page is new-secret; "/" and mismatches */}
+        <Route path="/new-secret" element={<NewSecret />} />
         <Route path="/" element={<Navigate to={"/new-secret"} replace />} />
-        <Route path="/new-secret" element={<NewSecretForm />} />
-        <Route path="/open-secret/:secretID" element={<OpenSecret />} />
         <Route path="*" element={<Navigate to={"/new-secret"} replace />} />
+        {/* Open Secret Page; secretID url parameter */}
+        <Route path="/open-secret/:secretID" element={<OpenSecret />} />
       </Routes>
     </Layout>
   );
