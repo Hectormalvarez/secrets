f9029946bc7d90087f2dfd64de0db5dae3ee75b1
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-08-31 06:37:27 -0500

    mocked react router usenavigate function

--- src/pages/OpenSecret.test.tsx
+++ src/pages/OpenSecret.test.tsx
@@ -1,9 +1,16 @@
+import React from "react";
 import { render } from "@testing-library/react";
 import OpenSecret from "./OpenSecret";
 
 jest.mock("nanoid", () => {
   return { nanoid: () => "1234" };
 });
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom') as any,
+  useNavigate: () => mockedUsedNavigate,
+}));
 
 describe("<OpenSecret /> smoke test", () => {
   it("renders without error", () => {
