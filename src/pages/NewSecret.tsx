a10f730ed16b0a6d3a453dac244327c061078b04
hadev <halvarez@taylormadetech.net>
2022-08-30 19:11:09 -0500

    added expiration to secret w uploaded

--- src/pages/NewSecret.tsx
+++ src/pages/NewSecret.tsx
@@ -15,7 +15,10 @@ const NewSecretForm = () => {
 
   const handleNewSecretSubmit = async (e: BaseSyntheticEvent) => {
     e.preventDefault();
+
+    // handle empty form submission
     if (e.target[0].value === "") {
+      // warn user that secret is required
       toast.warn("please enter a secret!");
       return;
     }
@@ -24,20 +27,29 @@ const NewSecretForm = () => {
     try {
       // create id for new secret
       const secretID = createID();
+      // create expiration date for secret (6 hours from current time)
+      const secretDate = new Date(); // get's current datetime
+      secretDate.setHours(secretDate.getHours() + 48) // adds 48hours
+      
+      debugger;
       // upload secret to the cloud
       await API.graphql(
         graphqlOperation(createSecret, {
-          input: { id: secretID, secretText: encryptedSecret },
+          input: {
+            id: secretID,
+            secretText: encryptedSecret,
+            expiration: secretDate,
+          },
         })
       );
       // save secretid in state
       setNewSecretID(secretID);
       // clear form
       e.target[0].value = "";
-      // notify user of success
+      // notify user of success uploading
       toast.success("secret uploaded to the cloud and link created!");
     } catch (error) {
-      // notify user of error
+      // notify user of error uploading
       toast.error("unable to upload secret!");
       console.log(error);
     }
@@ -52,27 +64,30 @@ const NewSecretForm = () => {
         secretID={newSecretID}
       />
       <form
-        className="relative m-2 flex h-full flex-col justify-end md:mx-auto md:max-w-3xl md:px-8"
+        className="m-2 flex h-full flex-col justify-end md:mx-auto md:max-w-3xl md:justify-start md:px-8"
         onSubmit={handleNewSecretSubmit}
       >
         <label className="text-center text-3xl font-bold tracking-wider text-slate-800 md:text-4xl">
           Enter a New Secret
         </label>
-        <textarea
-          className="mb-2 h-2/3 rounded-lg border-4 border-slate-800 bg-slate-100 p-2 text-lg tracking-tighter md:mb-6 md:h-3/4  md:text-2xl"
-          placeholder="Private information you want to share goes here"
-          onChange={(e) => setTextAreaLength(e.target.textLength)}
-          maxLength={500}
-        />
+        <div className="w-full h-3/5 relative mb-2">
+          <textarea
+            className="w-full h-full rounded-lg border-4 border-slate-800 bg-slate-100 p-2 text-lg tracking-tighter md:mb-6 md:text-2xl"
+            placeholder="Private information you want to share goes here"
+            onChange={(e) => setTextAreaLength(e.target.textLength)}
+            maxLength={500}
+          />
+          <p className="absolute bottom-4 right-8 text-lg md:text-2xl">
+            {textAreaLength}/500
+          </p>
+        </div>
         <button
           type="submit"
           className="mb-6 rounded-lg border-4 border-slate-800 bg-slate-500 py-4 text-2xl uppercase tracking-wider text-slate-200 shadow-md shadow-slate-800 md:py-6 md:text-4xl md:font-bold md:hover:bg-slate-400 md:hover:text-slate-100 md:hover:shadow-slate-700"
         >
           Create New Secret
         </button>
-        <p className="absolute bottom-32 right-5 text-lg md:bottom-40 md:right-16 md:text-2xl">
-          {textAreaLength}/500
-        </p>
+        <p className="text-2xl uppercase font-bold tracking-wider">secrets expire after 48 hours!</p>
       </form>
     </>
   );
