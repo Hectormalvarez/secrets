b67c554e36adbad940f01e0edce3391e27d3dee1
hadev <halvarez@taylormadetech.net>
2022-09-01 16:27:27 -0500

    formatting fixes

--- src/components/modal/NewSecretModal.tsx
+++ src/components/modal/NewSecretModal.tsx
@@ -44,7 +44,10 @@ export default function NewSecretModal({ isOpen, setIsOpen, secretID }: any) {
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog
         as="div"
-        className="relative z-10"
+        className="
+          relative
+          z-10
+        "
         onClose={!copied ? handleClickOutsideModal : closeModal}
       >
         <Transition.Child
@@ -56,14 +59,39 @@ export default function NewSecretModal({ isOpen, setIsOpen, secretID }: any) {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
+          {/* screen overlay */}
           <div
-            className="fixed inset-0 bg-gray-600 bg-opacity-90 transition-opacity"
+            className="
+              fixed
+              inset-0
+             bg-gray-600
+              bg-opacity-90
+              transition-opacity
+            "
             aria-hidden="true"
           />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center md:items-center md:p-0">
+        <div
+          className="
+              fixed
+              inset-0
+              z-10
+              overflow-y-auto
+            "
+        >
+          <div
+            className="
+              flex
+              min-h-full
+              items-end
+              justify-center
+              p-4
+              text-center
+              md:items-center
+              md:p-0
+            "
+          >
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-500"
@@ -73,24 +101,76 @@ export default function NewSecretModal({ isOpen, setIsOpen, secretID }: any) {
               leaveFrom="opacity-100 translate-y-0 md:scale-100"
               leaveTo="opacity-0 translate-y-4 md:translate-y-0 md:scale-95"
             >
-              <Dialog.Panel className="relative overflow-hidden rounded-md transition-all md:my-8 md:w-full md:max-w-lg">
-                <div className="bg-slate-100 px-4 pt-5 pb-4 md:p-6 md:pb-4">
-                  <div className="md:flex md:items-start ">
-                    <div className="mt-3 text-center md:mt-0 md:ml-4 md:flex-grow md:text-left">
+              <Dialog.Panel
+                className="
+                  relative
+                  overflow-hidden
+                  rounded-md
+                  transition-all
+                  md:my-8
+                  md:w-full
+                  md:max-w-lg
+                "
+              >
+                <div
+                  className="
+                   bg-slate-100
+                    px-4
+                    pt-5
+                    pb-4
+                    md:p-6
+                    md:pb-4
+                  "
+                >
+                  <div
+                    className="
+                      md:flex
+                      md:items-start
+                    "
+                  >
+                    <div
+                      className="
+                        mt-3
+                        text-center
+                        md:mt-0
+                        md:ml-4
+                        md:flex-grow
+                        md:text-left
+                      "
+                    >
                       <Dialog.Title
                         as="h3"
-                        className="text-3xl font-bold leading-6 text-gray-900"
+                        className="
+                          text-3xl
+                          font-bold
+                          leading-6
+                          text-gray-900
+                        "
                       >
                         New Secret Created!
                       </Dialog.Title>
-                      <div className="mt-2 md:text-xl">
+                      <div
+                        className="
+                          mt-2
+                          md:text-xl
+                        "
+                      >
                         share your one time secret with this link!
                       </div>
                       <input
                         type="text"
                         name="new-secret-link"
                         id="new-secret-link"
-                        className="mt-2 w-full overflow-y-auto rounded-lg bg-slate-800 p-2 text-sm text-slate-200 "
+                        className="
+                          mt-2
+                          w-full
+                          overflow-y-auto
+                          rounded-lg
+                          bg-slate-800
+                          p-2
+                          text-sm
+                          text-slate-200
+                        "
                         value={`${window.location.origin}/open-secret/${secretID}`}
                         onClick={handleSecretLinkClick}
                         readOnly
@@ -102,23 +182,72 @@ export default function NewSecretModal({ isOpen, setIsOpen, secretID }: any) {
                   <button
                     type="button"
                     className="
-                      md:text-lg flex w-full items-center justify-center rounded-md border-4 border-white bg-slate-500 px-4 py-2 text-white shadow-md
-                     hover:bg-blue-100 md:ml-3 md:w-auto md:hover:text-slate-700 md:font-bold tracking-wide md:transition-all md:duration-300 md:hover:scale-105
+                      flex
+                      w-full
+                      items-center
+                      justify-center
+                      rounded-md
+                      border-4
+                      border-white
+                      bg-slate-500
+                      px-4
+                      py-2
+                      tracking-wide
+                      text-white
+                      shadow-md
+                     hover:bg-blue-100
+                      md:ml-3
+                      md:w-auto
+                      md:text-lg
+                      md:font-bold
+                      md:transition-all
+                      md:duration-300
+                      md:hover:scale-105
+                      md:hover:text-slate-700
                     "
                     onClick={copySecretLink}
                   >
                     <p className="grow">Copy to Clipboard!</p>
-                    <ClipboardCheckIcon className="h-8 w-8" />
+                    <ClipboardCheckIcon
+                      className="
+                        h-8
+                        w-8
+                      "
+                    />
                   </button>
                   <button
                     type="button"
                     className={`
-                      mt-3 flex w-full items-center justify-center rounded-md border-4 px-4 py-2 md:text-lg md:font-bold
-                     md:mt-0 md:ml-3 md:w-auto 
+                      mt-3
+                      flex
+                      w-full
+                      items-center
+                      justify-center
+                      rounded-md
+                      border-4
+                      px-4
+                      py-2
+                      md:mt-0
+                      md:ml-3
+                      md:w-auto
+                      md:text-lg
+                      md:font-bold 
                      ${
                        copied
-                         ? "border-gray-700 bg-gray-200 text-gray-700 md:cursor-pointer md:hover:bg-slate-400 md:hover:text-white md:transition-all md:duration-300 md:hover:scale-105"
-                         : "border-gray-300 bg-gray-400 text-gray-500 md:cursor-not-allowed"
+                         ? `border-gray-700
+                            bg-gray-200
+                            text-gray-700
+                            md:cursor-pointer
+                            md:transition-all
+                            md:duration-300
+                            md:hover:scale-105
+                            md:hover:bg-slate-400
+                            md:hover:text-white`
+                         
+                            : `border-gray-300
+                               bg-gray-400
+                               text-gray-500
+                                md:cursor-not-allowed`
                      }
                     `}
                     onClick={closeModal}
