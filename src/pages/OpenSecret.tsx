ed18db6299d0ddaeef5ab88a353a4a1a2cc6edbc
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-09-03 11:10:25 -0500

    removing more functionality

--- src/pages/OpenSecret.tsx
+++ src/pages/OpenSecret.tsx
@@ -1,40 +1,18 @@
-import React, { useEffect, useRef, useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-
-import { toast } from "react-toastify";
+import React, { useRef } from "react";
 
 import { API, graphqlOperation } from "aws-amplify";
 import { getSecret } from "../graphql/queries";
 import { deleteSecret } from "../graphql/mutations";
 
-import { decryptText } from "../utils";
-
 
 const OpenSecret = () => {
   const secretTextRef = useRef<HTMLTextAreaElement>(null);
-  const { secretID } = useParams();
 
-
-  const handleSecretClick = () => {
-
-  };
-
-  const handleOpenSecret = () => {
-
-  };
-
-  // function to retreive secret from cloud
   const downloadSecret = async (secretID: string) => {
-    try {
-      const secretData: any = await API.graphql(
-        graphqlOperation(getSecret, { id: secretID })
-      );
-      return secretData.data.getSecret.secretText;
-    } catch (error) {
-      toast.error("Unable to Download Secret", {
-        toastId: "download-secret-error",
-      });
-    }
+    const secretData: any = await API.graphql(
+      graphqlOperation(getSecret, { id: secretID })
+    );
+    return secretData.data.getSecret.secretText;
   };
 
   // function for deleting secrets from cloud
@@ -43,10 +21,6 @@ const OpenSecret = () => {
       graphqlOperation(deleteSecret, { input: { id: secretID } })
     );
   };
-
-  useEffect(() => {
-    console.log(secretID)
-  }, [secretID]);
 
   return (
     <section
@@ -62,37 +36,27 @@ const OpenSecret = () => {
       </h2>
       <textarea
         className="open-secret-ta"
-        onClick={handleSecretClick} // copies text to clipboard if open
         ref={secretTextRef}
         readOnly
       />
-      <OpenSecretButton
-        handleOpenSecret={handleOpenSecret}
-        handleSecretClick={handleSecretClick}
-      />
-      <CreateNewSecretButton
-      />
+      <OpenSecretButton />
+      <CreateNewSecretButton />
     </section>
   );
 };
 
-const CreateNewSecretButton = ({ }: any) => {
-  let navigate = useNavigate();
+const CreateNewSecretButton = () => {
   return (
     <button
       type="submit"
       className="create-new-secret button"
-      onClick={() => {
-        navigate("/new-secret", { replace: true });
-      }}
     >
       Create New Secret!
     </button>
   );
 };
 
-const OpenSecretButton = ({ }: any) => {
-  // if secret contain unable to download error show create secret button
+const OpenSecretButton = () => {
   return (
     <button
       type="submit"
