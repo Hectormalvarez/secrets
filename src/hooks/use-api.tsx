e32700d53487c562a333863d8a23a4cfb9993343
Hector Alvarez <alvarez.hectorm@outlook.com>
2022-09-04 11:02:06 -0500

    fixed api hook error handling and data setting

--- src/hooks/use-api.tsx
+++ src/hooks/use-api.tsx
@@ -4,19 +4,23 @@ import { API, graphqlOperation } from "aws-amplify";
 
 const useAPI = () => {
   const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const [error, setError] = useState<null | string>()
 
-  const sendAPICall = useCallback(async (input: {}, graphqlOp: any) => {
+  const sendAPICall = useCallback(async (input: {}, graphqlOp: any, setData: any) => {
     setIsLoading(true);
     setError(null);
     try {
       const APICallResponse: any = await API.graphql(
-        graphqlOperation(graphqlOp, { input })
+        graphqlOperation(graphqlOp, input)
       );
-      return APICallResponse;
+      if (!APICallResponse.data.getSecret) {
+        throw new Error("Secret not found!");
+      }
+      setData(APICallResponse.data)
     } catch (error: any) {
       setError(error.message || 'Something went wrong!')
     }
+    setIsLoading(false);
   }, []);
 
   return {
