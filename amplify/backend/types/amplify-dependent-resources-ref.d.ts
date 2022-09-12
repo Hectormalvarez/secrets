311816f9d04490718353e098d523636fe96d4d3e
hadev <halvarez@taylormadetech.net>
2022-09-12 17:52:15 -0500

    misc amplify updates

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,7 +1,18 @@
 export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "secrets073a3f63073a3f63": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
     "api": {
         "secrets": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
