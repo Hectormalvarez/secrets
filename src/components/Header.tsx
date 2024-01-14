931f2e867cb2e502a67ac5079183a3d3e25a0f07
Hector Alvarez <alvarez.hectorm@outlook.com>
2024-01-14 23:13:20 +0000

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import VaultLogo from "@/logo1.svg";
 
 const Header = () => {
   return (
-    <header className="bg-white shadow-md py-4 text-gray-800">
-      <div className="container mx-auto px-4 flex items-center justify-between">
-        <div className="flex items-center">
-          <Link href="/">Vault</Link>
+    <header className="bg-white p-6 text-gray-800 shadow-md">
+      <div className="flex h-16 items-center">
+        <div className="w-16 rounded-full">
+          <Link href="/">
+            <Image src={VaultLogo} alt="Vault Logo" className="rounded-lg" />
+          </Link>
         </div>
+        <p className="px-8 text-5xl font-serif uppercase">Vault</p>
       </div>
     </header>
   );
